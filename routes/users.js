b40ccd2e49aa6428efc08c6edd8b4af839b5511a
Serena Celeste <serenaceleste@gmail.com>
2021-07-03 13:04:10 -0400

    Week 3 Workshop

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,7 @@ const authenticate = require('../authenticate');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
     res.send('respond with a resource');
 });
 
